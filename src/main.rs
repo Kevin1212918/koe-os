b95d35979224af18cb4663050c4730addd5ca176
Kevin Wu <kevinwu2@andrew.cmu.edu>
2024-09-15 19:53:14 -0400

    Implement bootstrap into long mode

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 
 mod multiboot2_header; 
 
-use core::panic::PanicInfo;
+use core::{arch::global_asm, panic::PanicInfo};
+
+global_asm!(include_str!("boot.S"));
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -13,7 +15,7 @@ fn panic(_info: &PanicInfo) -> ! {
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn kmain() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
 
     for (i, &byte) in HELLO.iter().enumerate() {
